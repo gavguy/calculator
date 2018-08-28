00b81f1b01d1af6c93cf50d06d0ba8ed617596bc
Andrej_Tesakov <gavguy3@gmail.com>
2018-08-28 18:40:00 +0300

    теперь можно считать (+) и (-) очень много раз

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -11,55 +11,23 @@ import java.sql.SQLOutput;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
-
-        double result;
-
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "error";
-        }
-        if (expression.length < 4) {
-            return String.valueOf(result);
-        }
-        System.out.println(expression[3]);
-        System.out.println(expression[4]);
-        double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "error";
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "error";
+            }
         }
 
         return String.valueOf(result);
-    }
 
+    }
 }
