675a944b6e21b8af88fc40a4808a18570b45b1e5
Andrej_Tesakov <gavguy3@gmail.com>
2018-09-11 19:16:48 +0300

    dobalneno izmemenija dlja skobok, teper mozno mnogo skobok vstovljatj i ERROR ne budet!!!!!

--- src/calculator/calc_umnoz.java
+++ src/calculator/calc_umnoz.java
@@ -14,66 +14,73 @@ class Calculator {
     String calculate(String[] expression) {
         List<String> tmp = new ArrayList<>(List.of(expression));///1.---var tmp = , mozno i tak v 10win.
 
+
         int brOpenids = -1;
-        for (int i = 0; i < tmp.size(); i++) {  ///3.--- fori + tab
-            String a = tmp.get(i);
-            if (a.equals("(")) {
-                brOpenids = i;
-            } else if (a.equals(")")) {
-                var inbr = tmp.subList(brOpenids + 1, i);
-                var result = calculate(inbr); ///4.---resultat na alculirovanije
-                tmp.subList(brOpenids, i + 1).clear();
-                tmp.add(brOpenids, result);
-
-            }
-        }
-            return calculate(tmp); ////2. ---pdgotovilis k skobockam
-
-
-        }
-        String calculate (List < String > expression) {    ///spisok strok
-            List<String> tmp = new ArrayList<>();
-            double a = Double.parseDouble(expression.get(0));
-            for (int i = 1; i < expression.size(); i += 2) {
-                String op = expression.get(i);
-                double b = Double.parseDouble(expression.get(1 + 1));
-                switch (op) {
-                    case "+":
-                    case "-":
-                        tmp.add(String.valueOf(a));
-                        tmp.add(op);
-                        a = b;
-                        break;
-                    case "*":
-                        a *= b;
-                        break;
-                    case "/":
-                        a /= b;
-                        break;
+        do {
+            brOpenids = -1;
+            for (int i = 0; i < tmp.size(); i++) {  ///3.--- fori + tab = sozdatj
+                String a = tmp.get(i);
+                if (a.equals("(")) {
+                    brOpenids = i;
+                } else if (a.equals(")")) {
+                    var inbr = tmp.subList(brOpenids + 1, i);
+                    var result = calculate(inbr); ///4.---resultat na alculirovanije
+                    tmp.subList(brOpenids, i + 1).clear();
+                    tmp.add(brOpenids, result);
+                    break;
                 }
             }
 
-            tmp.add(String.valueOf(a));
+        } while (brOpenids != -1);
 
-            System.out.println(tmp);
+        return calculate(tmp); ////2. ---pdgotovilis k skobockam
 
-            double result = Double.parseDouble(tmp.get(0));
-            for (int i = 1; i < tmp.size(); i += 2) {
-                String op = tmp.get(i);
-                double b = Double.parseDouble(tmp.get(i + 1));
-                switch (op) {
-                    case "+":
-                        result += b;
-                        break;
-                    case "-":
-                        result -= b;
-                        break;
-                    default:
-                        return "ERROR";
-                }
-            }
-
-            return String.valueOf(result);
-        }
 
     }
+
+    String calculate(List<String> expression) {    ///spisok strok
+        List<String> tmp = new ArrayList<>();
+        double a = Double.parseDouble(expression.get(0));
+        for (int i = 1; i < expression.size(); i += 2) {
+            String op = expression.get(i);
+            double b = Double.parseDouble(expression.get(1 + 1));
+            switch (op) {
+                case "+":
+                case "-":
+                    tmp.add(String.valueOf(a));
+                    tmp.add(op);
+                    a = b;
+                    break;
+                case "*":
+                    a *= b;
+                    break;
+                case "/":
+                    a /= b;
+                    break;
+            }
+        }
+
+        tmp.add(String.valueOf(a));
+
+        System.out.println(tmp);
+
+        double result = Double.parseDouble(tmp.get(0));
+        for (int i = 1; i < tmp.size(); i += 2) {
+            String op = tmp.get(i);
+            double b = Double.parseDouble(tmp.get(i + 1));
+            switch (op) {
+                case "+":
+                    result += b;
+                    break;
+                case "-":
+                    result -= b;
+                    break;
+                default:
+                    return "ERROR";
+            }
+        }
+
+        return String.valueOf(result);
+    }
+
+}
