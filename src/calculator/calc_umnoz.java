9ec8a8766a45087bb3229fb0fa3cf3c00f592443
Andrej_Tesakov <gavguy3@gmail.com>
2018-09-11 18:44:16 +0300

    добавил скобочкиб теперь колькулятор умеет считать со Э()Э

--- src/calculator/calc_umnoz.java
+++ src/calculator/calc_umnoz.java
@@ -12,48 +12,68 @@ import java.util.List;
 class Calculator {
 
     String calculate(String[] expression) {
-        List<String> tmp = new ArrayList<>();
-        double a = Double.parseDouble(expression[0]);
-        for (int i = 1; i < expression.length; i+=2) {
-            String op = expression[i];
-            double b = Double.parseDouble(expression[i + 1]);
-            switch (op) {
-                case "+":
-                case "-":
-                    tmp.add(String.valueOf(a));
-                    tmp.add(op);
-                    a = b;
-                    break;
-                case "*":
-                    a *= b;
-                    break;
-                case "/":
-                    a /= b;
-                    break;
+        List<String> tmp = new ArrayList<>(List.of(expression));///1.---var tmp = , mozno i tak v 10win.
+
+        int brOpenids = -1;
+        for (int i = 0; i < tmp.size(); i++) {  ///3.--- fori + tab
+            String a = tmp.get(i);
+            if (a.equals("(")) {
+                brOpenids = i;
+            } else if (a.equals(")")) {
+                var inbr = tmp.subList(brOpenids + 1, i);
+                var result = calculate(inbr); ///4.---resultat na alculirovanije
+                tmp.subList(brOpenids, i + 1).clear();
+                tmp.add(brOpenids, result);
+
             }
         }
+            return calculate(tmp); ////2. ---pdgotovilis k skobockam
 
-        tmp.add(String.valueOf(a));
 
-        System.out.println(tmp);
-
-        double result = Double.parseDouble(tmp.get(0));
-        for (int i = 1; i < tmp.size(); i += 2) {
-            String op = tmp.get(i);
-            double b = Double.parseDouble(tmp.get(i + 1));
-            switch (op) {
-                case "+":
-                    result += b;
-                    break;
-                case "-":
-                    result -= b;
-                    break;
-                default:
-                    return "ERROR";
+        }
+        String calculate (List < String > expression) {    ///spisok strok
+            List<String> tmp = new ArrayList<>();
+            double a = Double.parseDouble(expression.get(0));
+            for (int i = 1; i < expression.size(); i += 2) {
+                String op = expression.get(i);
+                double b = Double.parseDouble(expression.get(1 + 1));
+                switch (op) {
+                    case "+":
+                    case "-":
+                        tmp.add(String.valueOf(a));
+                        tmp.add(op);
+                        a = b;
+                        break;
+                    case "*":
+                        a *= b;
+                        break;
+                    case "/":
+                        a /= b;
+                        break;
+                }
             }
+
+            tmp.add(String.valueOf(a));
+
+            System.out.println(tmp);
+
+            double result = Double.parseDouble(tmp.get(0));
+            for (int i = 1; i < tmp.size(); i += 2) {
+                String op = tmp.get(i);
+                double b = Double.parseDouble(tmp.get(i + 1));
+                switch (op) {
+                    case "+":
+                        result += b;
+                        break;
+                    case "-":
+                        result -= b;
+                        break;
+                    default:
+                        return "ERROR";
+                }
+            }
+
+            return String.valueOf(result);
         }
 
-        return String.valueOf(result);
     }
-
-}
